a4aba84451725e40b03f480bb42c9169c75ca851
gprose1234gmail <95291555+gprose1234gmail@users.noreply.github.com>
2022-04-12 08:03:29 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -8,17 +8,10 @@
 # All rights reserved.
 
 from pyrogram.types import InlineKeyboardButton
-from Yukki import db_mem
 
-                   
-  def track_markup(_, videoid, user_id, channel, fplay,current_time, total_time):
+
+def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
-          [
-            InlineKeyboardButton(
-                text=f"{total_time} ------------------ {current_time}",
-                callback_data=f"timer_checkup_markup {videoid}|{user_id}",
-            )
-        ],
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
@@ -39,14 +32,8 @@ from Yukki import db_mem
     return buttons
 
 
-def stream_markup(_, videoid,current_time, total_time,user_id):
+def stream_markup(_, videoid):
     buttons = [
-          [
-            InlineKeyboardButton(
-                text=f"{total_time} ------------------ {current_time}",
-                callback_data=f"timer_checkup_markup {videoid}|{user_id}",
-            )
-        ],
         [
             InlineKeyboardButton(
                 text=_["PL_B_2"],
